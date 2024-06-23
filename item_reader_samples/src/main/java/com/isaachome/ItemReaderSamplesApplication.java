ee0fd876823bc3236073fb8a593bcd07582dd5fa
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-06-23 23:41:21 +0800

    clean-up project structure

---
+++ item_reader_samples/src/main/java/com/isaachome/ItemReaderSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.isaachome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemReaderSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemReaderSamplesApplication.class, args);
+    }
+
+}
